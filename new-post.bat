4a1893846b65ffdd464244796a784f531d9d3e57
lxm <2646605638@qq.com>
2025-07-09 01:33:44 +0800

    优化新建博客

--- new-post.bat
+++ new-post.bat
@@ -18,9 +18,28 @@ echo category: '技术'>> src/content/blog/%filename%.mdx
 echo draft: false>> src/content/blog/%filename%.mdx
 echo --->> src/content/blog/%filename%.mdx
 echo.>> src/content/blog/%filename%.mdx
-echo 在这里开始写文章内容...>> src/content/blog/%filename%.mdx
 
-echo 已创建文章模板：src/content/blog/%filename%.mdx
+:: 添加资源博客模板
+echo # %title%>> src/content/blog/%filename%.mdx
+echo.>> src/content/blog/%filename%.mdx
+echo ## 1. 第一部分>> src/content/blog/%filename%.mdx
+echo.>> src/content/blog/%filename%.mdx
+echo ^<div class="resource-item"^>>> src/content/blog/%filename%.mdx
+echo   ^<a href="https://example.com" class="resource-link"^>示例链接^</a^>>> src/content/blog/%filename%.mdx
+echo   ^<div class="resource-url"^>https://example.com^</div^>>> src/content/blog/%filename%.mdx
+echo   ^<div class="resource-desc"^>这是一个示例描述，请替换为实际内容^</div^>>> src/content/blog/%filename%.mdx
+echo ^</div^>>> src/content/blog/%filename%.mdx
+echo.>> src/content/blog/%filename%.mdx
+echo ## 2. 第二部分>> src/content/blog/%filename%.mdx
+echo.>> src/content/blog/%filename%.mdx
+echo ^<div class="resource-item"^>>> src/content/blog/%filename%.mdx
+echo   ^<a href="https://example2.com" class="resource-link"^>另一个示例^</a^>>> src/content/blog/%filename%.mdx
+echo   ^<div class="resource-url"^>https://example2.com^</div^>>> src/content/blog/%filename%.mdx
+echo   ^<div class="resource-desc"^>这是第二个示例描述^</div^>>> src/content/blog/%filename%.mdx
+echo ^</div^>>> src/content/blog/%filename%.mdx
+echo.>> src/content/blog/%filename%.mdx
+
+echo 已创建资源博客模板：src/content/blog/%filename%.mdx
 echo 当前日期: %today%
 echo 现在您可以编辑这个文件开始写博客了！
 pause 
