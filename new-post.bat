f0d75824af5b1ebb440e2b713379bb1b654c764c
lxm <2646605638@qq.com>
2025-07-09 00:51:45 +0800

    新建博客资源整合，优化新建博客脚本

--- new-post.bat
+++ new-post.bat
@@ -1,15 +1,18 @@
 @echo off
 chcp 65001 > nul
 set /p title=请输入文章标题: 
+set /p description=请输入文章描述: 
 set /p filename=请输入文件名(不含后缀): 
 
 echo --->> src/content/blog/%filename%.mdx
 echo title: '%title%'>> src/content/blog/%filename%.mdx
-echo description: ''>> src/content/blog/%filename%.mdx
+echo description: '%description%'>> src/content/blog/%filename%.mdx
 echo pubDate: %date:~0,4%-%date:~5,2%-%date:~8,2%>> src/content/blog/%filename%.mdx
-echo heroImage: '/assets/images/1.jpg'>> src/content/blog/%filename%.mdx
+echo updatedDate: %date:~0,4%-%date:~5,2%-%date:~8,2%>> src/content/blog/%filename%.mdx
+echo heroImage: '/assets/images/6.jpg'>> src/content/blog/%filename%.mdx
+echo tags: ['Markdown', '指南', '教程']>> src/content/blog/%filename%.mdx
 echo category: '技术'>> src/content/blog/%filename%.mdx
-echo tags: ['标签1']>> src/content/blog/%filename%.mdx
+echo draft: false>> src/content/blog/%filename%.mdx
 echo --->> src/content/blog/%filename%.mdx
 echo.>> src/content/blog/%filename%.mdx
 echo 在这里开始写文章内容...>> src/content/blog/%filename%.mdx
